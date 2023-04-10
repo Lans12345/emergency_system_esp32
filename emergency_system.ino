3f700c3013d5bfdbdccbe71ab9e3528c2771533f
Lans12345 <olanalans12345@gmail.com>
2023-04-10 17:23:44 +0800

    added catche statement

--- emergency_system.ino
+++ emergency_system.ino
@@ -1,63 +1,42 @@
-#include <AG9.h>
+#include <SoftwareSerial.h>
 
-AG9 gps;
-int buzzer = 7;
-int button1 = 8;
-int button2 = 9;
+SoftwareSerial mySerial(8, 9); // RX, TX
 
 void setup() {
-  pinMode(buzzer, OUTPUT);
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-  
   Serial.begin(9600);
-  gps.begin();
+  mySerial.begin(9600);
+
+  pinMode(12, INPUT_PULLUP); // Button for making phone call
+  pinMode(13, INPUT_PULLUP); // Button for sending SMS message
 }
 
 void loop() {
-  if (digitalRead(button1) == HIGH) {
-    makeCall("+639639530422"); //replace with your desired phone number
+  // Make phone call when button 1 is pressed
+  if (digitalRead(12) == LOW) {
+    mySerial.println("ATD+639639530422;"); // Replace with the phone number you want to call
+    delay(5000); // Wait for 5 seconds
+    mySerial.println("ATH"); // Hang up the call
   }
-  
-  if (digitalRead(button2) == HIGH) {
-    sendSMS("+639639530422", getLocationMessage()); //replace with your desired phone number
+
+  // Send SMS message when button 2 is pressed
+  if (digitalRead(13) == LOW) {
+    mySerial.println("AT+CMGF=1"); // Set the SMS mode to text
+    delay(1000);
+    mySerial.print("AT+CMGS=\"+639639530422\"\r"); // Replace with the phone number you want to send SMS to
+    delay(1000);
+    mySerial.print("Help! I need assistance."); // Replace with the message you want to send
+    mySerial.write(26); // End the SMS message with Ctrl+Z
+    delay(10000); // Wait for the message to be sent
+
+    if (mySerial.find("OK")) {
+      Serial.println("Text message sent successfully.");
+    } else {
+      Serial.println("Error sending text message.");
+    }
   }
-}
 
-void makeCall(String phoneNumber) {
-  digitalWrite(buzzer, HIGH);
-  delay(500);
-  digitalWrite(buzzer, LOW);
-  delay(500);
-  
-  gps.call(phoneNumber);
-  
-  delay(10000); //wait for 10 seconds before allowing another call
-}
-
-void sendSMS(String phoneNumber, String message) {
-  digitalWrite(buzzer, HIGH);
-  delay(500);
-  digitalWrite(buzzer, LOW);
-  delay(500);
-  
-  gps.enableGPS();
-  delay(2000); //wait for GPS to stabilize
-  
-  float latitude = gps.getLatitude();
-  float longitude = gps.getLongitude();
-  
-  gps.disableGPS();
-  
-  String location = String(latitude, 6) + "," + String(longitude, 6);
-  message = message + " My location is: " + location;
-  
-  gps.sendSMS(phoneNumber, message);
-  
-  delay(10000); //wait for 10 seconds before sending another SMS
-}
-
-String getLocationMessage() {
-  //customize your message here
-  return "Help me, I'm in danger!";
+  // Check for incoming data from the A9G module
+  while (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
 }
