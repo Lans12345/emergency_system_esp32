df622f969dfa9e3013f0e2c40850b72071cf9a17
Lans12345 <olanalans12345@gmail.com>
2023-04-10 19:30:50 +0800

    initial progress

--- emergency_system.ino
+++ emergency_system.ino
@@ -6,24 +6,28 @@ void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
 
-  pinMode(12, INPUT_PULLUP); // Button for making phone call
-  pinMode(13, INPUT_PULLUP); // Button for sending SMS message
+
+  pinMode(11, OUTPUT);
+
+  pinMode(12, INPUT); // Button for making phone call
+  pinMode(13, INPUT); // Button for sending SMS message
 }
 
 void loop() {
   // Make phone call when button 1 is pressed
-  if (digitalRead(12) == LOW) {
-    mySerial.println("ATD+639639530422;"); // Replace with the phone number you want to call
-    delay(5000); // Wait for 5 seconds
+  if (digitalRead(12) == HIGH) {
+mySerial.println("ATD+639639530422;");// Replace with the phone number you want to call
+    delay(30000);
     mySerial.println("ATH"); // Hang up the call
   }
 
   // Send SMS message when button 2 is pressed
-  if (digitalRead(13) == LOW) {
+  if (digitalRead(13) == HIGH) {
+    digitalWrite(11, HIGH);
     mySerial.println("AT+CMGF=1"); // Set the SMS mode to text
     delay(1000);
-    mySerial.print("AT+CMGS=\"+639639530422\"\r"); // Replace with the phone number you want to send SMS to
-    delay(1000);
+mySerial.print("AT+CMGS=\"+639639530422\"");// Replace with the phone number you want to send SMS to
+    delay(10000);
     mySerial.print("Help! I need assistance."); // Replace with the message you want to send
     mySerial.write(26); // End the SMS message with Ctrl+Z
     delay(10000); // Wait for the message to be sent
@@ -31,6 +35,7 @@ void loop() {
     if (mySerial.find("OK")) {
       Serial.println("Text message sent successfully.");
     } else {
+Serial.println(mySerial.readString().c_str());
       Serial.println("Error sending text message.");
     }
   }
